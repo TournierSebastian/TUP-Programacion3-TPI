fd9f92d396df2365b1bcedc26c893bffc078b3ac
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2023-10-17 10:24:01 -0300

    Capas

---
+++ AplicacionWeb/Models/Models/SuperAdmin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class SuperAdmin: User
+    {
+        public ICollection<Admin> AdminModified { get; set; } = new List<Admin>();
+
+    }
+}
